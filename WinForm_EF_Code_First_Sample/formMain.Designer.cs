6d42aaa7e54cc54e1d225361ae8d02730d5fc1ad
Haydar ŞAHİN <haydarsahin34@hotmail.com>
2017-06-17 22:32:39 +0300

    Project created.

--- WinForm_EF_Code_First_Sample/formMain.Designer.cs
+++ WinForm_EF_Code_First_Sample/formMain.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace WinForm_EF_Code_First_Sample
 {
-    partial class Form1
+    partial class formMain
     {
         /// <summary>
         /// Required designer variable.
@@ -28,12 +28,19 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // formMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(829, 429);
+            this.Name = "formMain";
+            this.Text = "formMain";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
     }
 }
-
